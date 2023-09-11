8886c44ce558bc34349894b30af0265efbebb38d
Bernardo <Bernardo@DESKTOP-D90VCMQ>
2023-09-10 23:53:43 -0300

    act 11.53

--- fun.m
+++ fun.m
@@ -24,12 +24,19 @@ function [f, dx,dy] = fun(ecuacion, variables)
     a = 0 + dx ;
     b = 0 + dy ;
 
-    prim_eval = subs(ecuacion, [x, y], [a, b]);
+    prim_eval = subs(f, [x, y], [a, b]);
     disp(prim_eval)
 
+    valor_evaluado = subs(prim_eval, [x, y], [0, 0]);
+    %valor_evaluado = subs(rt,y,0);
+    disp(valor_evaluado);
     
+    [a, b, m] = biparticion(0, 2, 1e-6, @(u) subs(diff(valor_evaluado,r), [r], [u]))
+    fprintf ("El mu optimo para la funcion es %d\n\n", m(end) )
     
-    
+    %resol = diff(valor_evaluado) == 0;
+    %solucion = solve(resol, r);
+    %disp(solucion)
 end
 
 
